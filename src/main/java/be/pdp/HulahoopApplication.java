9dc60aab5fae8bc5531faea0599c1dee17386477
peterdp <peter.de.permentier@gmail.com>
2017-03-19 10:38:55 +0100

    initial commit

---
+++ src/main/java/be/pdp/HulahoopApplication.java
@@ -0,0 +1,12 @@
+package be.pdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulahoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulahoopApplication.class, args);
+	}
+}
